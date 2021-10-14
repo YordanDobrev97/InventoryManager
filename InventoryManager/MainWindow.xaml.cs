76cdab79075c037e30ce2ebd196945b61f9fde43
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-10-15 00:22:38 +0300

    Implement refresh UI

--- InventoryManager/MainWindow.xaml.cs
+++ InventoryManager/MainWindow.xaml.cs
@@ -1,20 +1,8 @@
-﻿using InventoryManager.Data.Models;
-using InventoryManager.Data.Repositories;
-using InventoryManager.ViewModels;
+﻿using InventoryManager.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InventoryManager
 {
@@ -32,11 +20,16 @@ namespace InventoryManager
 
         private void AddItem(object sender, RoutedEventArgs e)
         {
-            var viewModel = DataContext as MainViewModel;
+            var viewModel = this.DataContext as MainViewModel;
             viewModel.SelectedCategory.Items.Add(new ItemViewModel()
             {
                 Name = "New Item",
             });
+        }
+
+        private void Refresh(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = new MainViewModel();
         }
     }
 }
